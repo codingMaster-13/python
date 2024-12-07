528ca944c00d51e0e739ab933763009ccdf96036
codingMaster-13 <codingmaster035@gmail.com>
2024-12-07 10:39:58 +0530

    some changes

--- TodoList/main.py
+++ TodoList/main.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index_roue():
     return render_template('index.html')
 
